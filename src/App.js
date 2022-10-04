5bab593fae3aba2a2b88a072536f5cc42bbe5e15
Djamila Baroudi <db@blksail.ai>
2022-10-04 16:47:11 +0200

    calls the components inside App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import GoalsList from "./components/Goals/GoalsList";
+import NewGoalForm from "./components/GoalsForm/NewGoalForm";
+import './App.css'
 
 function App() {
+  const itemsList = [
+    {
+      id: 'g1',
+      content: 'Finish the react course'
+    },
+    {
+      id: 'g2',
+      content: 'Finish the react course'
+    }
+  ]
   return (
-    <div >
-      
+    <div className="wrapper">
+      <NewGoalForm />
+      <GoalsList goalsList={ itemsList } />
     </div>
   );
 }
